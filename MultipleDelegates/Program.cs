e04417d5532a0aecd4e5e8a0b313511e37997dbc
caiomodena <caio.modena@gmail.com>
2018-12-11 20:01:36 -0200

    multiple delegates

---
+++ MultipleDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultipleDelegates
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1,double n2);
+        static void Main(string[] args)
+        {
+            double a=10;
+            double b=12;
+
+            BinaryNumericOperation operation=CalculationService.ShowSum;
+            operation+=CalculationService.ShowMax;
+
+            operation.Invoke(a,b);
+            //ou operation(a,b);
+        }
+    }
+}
