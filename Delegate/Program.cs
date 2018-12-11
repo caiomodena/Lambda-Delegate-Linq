942e7abd57bc7decd0492c9f9392b5291da3926b
caiomodena <caio.modena@gmail.com>
2018-12-11 19:55:09 -0200

    delegate simples

---
+++ Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate double BinaryNumericOperation(double n1,double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 10;
+            double B = 12;
+
+            BinaryNumericOperation op = CalculationServices.Sum(A,B); 
+            double result=op(A,B);
+            Console.WriteLine(result);
+
+            BinaryNumericOperation op1 = CalculationServices.Max; 
+            double result1=op1(A,B);
+            Console.WriteLine(result1);
+        }
+    }
+}
---
+++ Delegate/Services/CalculationService.cs
@@ -0,0 +1,13 @@
+namespace Delegate{
+    class CalculationServices{
+        public static double Max(double x, double y){
+            return (x>y)?x:y;
+        }
+        public static double Sum(double x, double y){
+            return x+y;
+        }
+        public static double Square(double x){
+            return x*x;
+        }
+    }
+}
