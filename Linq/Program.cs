b99e615f6f684352a4131b759927f7c80a05fc0e
caiomodena <caio.modena@gmail.com>
2019-01-02 21:22:00 -0200

    primeiro exemplo com linq

---
+++ Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Especificar o data source
+            int[] numbers= new int[]{2,3,4,5};
+
+            //Definir a expressão de consulta
+            var result = numbers.Where(x => x%2==0).Select(x => x*10);
+
+            //Executar a consulta
+            foreach(int x in result){
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
