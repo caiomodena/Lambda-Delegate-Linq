91867f0f67be35487e5de44e60da69331f2b04f5
caiomodena <caio.modena@gmail.com>
2019-01-18 09:19:53 -0200

    linq com notação similar à sql

---
+++ Linq-SQL/Entities/Category.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace LinqSQL{
+    class Category{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
